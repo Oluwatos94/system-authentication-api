d1b8df4851838d0ea34dbc463052d0422e154906
Oluwatos94 <akinbowat@gmail.com>
2024-09-25 22:33:14 +0100

    fix: api route

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RegisterController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
